6c24d1f5dc6c245fdf74b09cc5548b3da0d96e68
Trieu Do <tdo02@jaguar.tamu.edu>
2025-08-31 09:04:33 -0500

    refactor(home): restructure with nunjucks template

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/img');
     eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
         dir: {
