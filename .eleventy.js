c0e717ea1f779d6d9fa8b7a4e030d8cf17c0b721
T Do <tdo02@jaguar.tamu.edu>
2025-10-16 20:46:00 -0500

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,18 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/img');
     eleventyConfig.addPassthroughCopy('./src/css');
     eleventyConfig.addPassthroughCopy('./src/js');
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     return {
         dir: {
             input: "src",
             output: "public"
-        }
+        },
+        pathPrefix: "/test/"
+
     }
 }
